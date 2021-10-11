a79545030a1483f59a283d6aad7f2ed932255cb4
vincemilo <vincemilo@gmail.com>
2021-10-11 10:18:10 -0600

    Updated up to find method

--- app.rb
+++ app.rb
@@ -27,6 +27,8 @@ class Tree
     root
   end
 
+  # visualization method provided by fellow student
+
   def pretty_print(node = @root, prefix = '', is_left = true)
     pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
@@ -42,10 +44,51 @@ class Tree
       node.right.nil? ? node.right = Node.new(val) : insert(val, node.right)
     end
   end
+
+  def leftmost_leaf(node)
+    node = node.left until node.left.nil?
+    node
+  end
+
+  def delete(val, node = root)
+    return node if node.nil?
+
+    if val < node.data
+      node.left = delete(val, node.left)
+    elsif val > node.data
+      node.right = delete(val, node.right)
+    else
+      # if node has 1 or less children
+      return node.right if node.left.nil?
+      return node.left if node.right.nil?
+
+      # if node has 2+ children
+      leftmost_node = leftmost_leaf(node.right)
+      node.data = leftmost_node.data
+      node.right = delete(leftmost_node.data, node.right)
+    end
+    node
+  end
+
+  def find(val, node = root)
+    return node if node.nil? || node.data == val
+
+    if val < node.data
+      find(val, node.left)
+    else
+      find(val, node.right)
+    end
+  end
+
 end
 
-arr = [1, 2, 3, 7, 5, 6, 4]
+arr = [6, 2, 3, 7, 5, 1, 4]
 bst = Tree.new(arr)
 bst.pretty_print
 bst.insert(0)
 bst.pretty_print
+bst.delete(0)
+bst.pretty_print
+bst.delete(4)
+bst.pretty_print
+p bst.find(3)
